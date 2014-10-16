daea3b0317f579193194aef63c33d553c566038b
tazzon <tazzon@free.fr>
2014-10-16 17:05:28 +0200

    Ajout des textes

---
+++ dactylotest/text/en.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Les textes doivent comporter :
+— les apostrophes typographique (’ et non '),
+— les ligatures æ, Æ, œ et Œ,
+— les espaces insécables là ou ils sont nécessaire,
+— les majuscules accentuées.
+*/  
+
+//$textes[0]["txt"] = "";
+//$textes[0]["source"] = "";
+
+$textes[0]["txt"] = "The most important of all Egyptian myths was the myth of Osiris and Isis. It tells of the divine ruler Osiris, who was murdered by his jealous brother Set, a god often associated with chaos. Osiris' sister and wife Isis resurrected him so that he could conceive an heir, Horus. Osiris then entered the underworld and became the ruler of the dead. Once grown, Horus fought and defeated Set to become king himself. Set's association with chaos, and the identification of Osiris and Horus as the rightful rulers, provided a rationale…";
+$textes[0]["source"] = "http://en.wikipedia.org/wiki/Ancient_Egyptian_religion#Mythology";
+
+$textes[1]["txt"] = "Mathematics is used throughout the world as an essential tool in many fields, including natural science, engineering, medicine, and the social sciences. Applied mathematics, the branch of mathematics concerned with application of mathematical knowledge to other fields, inspires and makes use of new mathematical discoveries and sometimes leads to the development of entirely new mathematical disciplines, such as statistics and game theory. Mathematicians also engage in pure mathematics, or mathematics for its own sake, withou…";
+$textes[1]["source"] = "http://en.wikipedia.org/wiki/Mathematics";
+
+$textes[2]["txt"] = "Free will is the purported ability of agents to make choices free from constraints. Historically, the constraint of dominant concern has been the metaphysical constraint of determinism. The opposing positions within that debate are metaphysical libertarianism, the claim that determinism is false and thus that free will exists; and hard determinism, the claim that determinism is true and thus that free will does not exist. Both of these positions, which agree that causal determination is the relevant factor in the question of…";
+$textes[2]["source"] = "http://en.wikipedia.org/wiki/Free_will";
+
+//$textes[3]["txt"] = "";
+//$textes[3]["source"] = "";
+
+//$textes[4]["txt"] = "";
+//$textes[4]["source"] = "";
+
+//$textes[0]["txt"] = "";
+//$textes[0]["source"] = "";
+
+//$textes[0]["txt"] = "";
+//$textes[0]["source"] = "";
+
+//$textes[0]["txt"] = "";
+//$textes[0]["source"] = "";
+
+//$textes[0]["txt"] = "";
+//$textes[0]["source"] = "";
+
+?>
