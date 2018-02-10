e3de1f8ea9ac59a669b21b7de462069dc77b3a59
Johan Bonneau <lhaagounet@gmail.com>
2018-02-10 02:22:33 +0100

    Typo
    Accord dans un commentaire.

--- tridactyl/js/start.js
+++ tridactyl/js/start.js
@@ -7,7 +7,7 @@ document.getElementById("rd_txt").innerHTML = ""; // effacement du message javas
 //document.getElementById("demo_curseur").style.backgroundColor = cur_col;
 //document.getElementById("view_options").style.display = "none";
 //document.getElementById("d_replay").style.visibility = "hidden";
-/*var cur_mix = mix_colors(cur_col,cur_col_ghost) // c'est le mélange des 2 couleurs des curseur du fantôme et de la frappe
+/*var cur_mix = mix_colors(cur_col,cur_col_ghost) // c'est le mélange des 2 couleurs des curseurs du fantôme et de la frappe
 clean_ghost();
 if (GetCookie("apo_typ") == "true") document.getElementById("apo_typ").checked = true;  
 if (GetCookie("maj_acc") == "true") document.getElementById("maj_acc").checked = true;
