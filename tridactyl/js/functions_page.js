c2f23ea0adb102b4d0ccd9303d4c0767fd66301b
Johan Bonneau <lhaagounet@gmail.com>
2018-02-11 01:51:34 +0100

    Typo
    Proposition de corrections.

--- tridactyl/js/functions_page.js
+++ tridactyl/js/functions_page.js
@@ -1,5 +1,5 @@
 /*******************************************************************************
-Toutes les fonctions liées à la page pour le gestion des cadres des options, 
+Toutes les fonctions liées à la page pour la gestion des cadres des options, 
 demande d'un nouveau texte…
 *******************************************************************************/
 
@@ -19,7 +19,7 @@ var cur_checked = "checked";
 
 
 var description = "<p style=\"text-align:justify\">Leçons à partir de trigrammes les plus récurrents.<br/><br/>"
-                + "L’apprentissage existe en 4 jeux de leçons de longueur différentes répartis en suffisamment de trigrammes pour obtenir au moins 3, 7, 11 et 15 mots.<br/>" 
+                + "L’apprentissage existe en 4 jeux de leçons de longueur différente répartis en suffisamment de trigrammes pour obtenir au moins 3, 7, 11 et 15 mots.<br/>" 
                 + "À chacun de choisir la longueur de l’apprentissage qui lui convient pour apprendre :<br/>"
                 + "<br/>"
                 + "— 30 leçons de 3 mots, 4 à 5 minutes par jour pendant 1 mois ;<br/>"
@@ -27,10 +27,10 @@ var description = "<p style=\"text-align:justify\">Leçons à partir de trigramm
                 + "— 24 leçons de 11 mots, 12 à 15 minutes par jour à la pause déjeuner pendant 1 mois ;<br/>"            
                 + "— 19 leçons de 15 mots, 15 à 20 minutes par jour à la pause déjeuner pendant 1 mois.<br/>"
                 + "<br/>"
-                + "Un accroissement de la vitesse et de la précision de seulement 20% devrait libérer environ 1 heure par jour pour tous les jour de votre vie au bureau.<br/>"
+                + "Un accroissement de la vitesse et de la précision de seulement 20% devrait libérer environ 1 heure par jour pour tous les jours de votre vie au bureau.<br/>"
                 + "Un doublement de la vitesse devrait permettre de libérer 1/2 journée par jour de clavier.<br/>"
-                + "Même si pendant l’apprentissage votre vitesse se trouvait réduite de 50% cela devrait être compensée par moins de temps perdu en correction orthographique et une meilleur syntaxe.<br/>"
-                + "Un enfant qui apprendrait la dactylographie à l’aveugle dispose d’un excellent moyen pour réduire la dyslexie et améliorer les résultats scolaires en terme d’efficacité et de présentation et cela en seulement 30 jours d’apprentissage.</p>";
+                + "Même si pendant l’apprentissage votre vitesse se trouvait réduite de 50% cela devrait être compensé par moins de temps perdu en correction orthographique et une meilleure syntaxe.<br/>"
+                + "Un enfant qui apprendrait la dactylographie à l’aveugle dispose d’un excellent moyen pour réduire la dyslexie et améliorer les résultats scolaires en terme d’efficacité et de présentation, et cela en seulement 30 jours d’apprentissage.</p>";
 
 
 function change_type_lecon(a)
@@ -72,7 +72,7 @@ function new_text(a)
 	if (!a)
 		a=1;
 
-	// requète javascript pour récupérer numéro###credit###texte
+	// requête javascript pour récupérer numéro###credit###texte
 	/*var req = "new_text.php?t="+lecon_nb+"&l="+document.getElementById("lang").value;
 	if (document.getElementById("methode").value == "number")
 	{
@@ -233,7 +233,7 @@ function options(e)
 	var id_ = "";																																 // le nom temporaire de l'ID de l'input
 	var check_ = "";																															// l'état temporaire du checkbox de l'input
 	
-	// url pour la requete
+	// url pour la requête
 	var url = "save_pref.php?";
 
 	// un tableau de tous les inputs qui se trouvent dans les options
